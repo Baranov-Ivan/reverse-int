13f1adafd8ab7a635b966f4cbdb7483d92c0382c
baranov-ivan <vano95by@gmail.com>
2021-02-21 16:52:58 +0300

    feat: add reverse-int task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    let arrInt = n.toString().split('').reverse();
+    let badChars = 0;
+    for(let i = 0; i < arrInt.length; i++) {
+        if(arrInt[i] === '0') {
+            badChars++;
+        } else {
+            break;
+        }
+    }
+
+    arrInt.splice(0,badChars);
+    return parseInt(arrInt.join(''),10);
 }
